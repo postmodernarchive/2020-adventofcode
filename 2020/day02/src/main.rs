3674411360f4d6de69eca3a328d12ced292b753b
youngtrashbag <yuule.smeker@gmail.com>
2020-12-02 11:40:48 +0100

    day02: implement get_criteria function
    each line has criteria for "password" which I parse, and return as a
    tupel

--- 2020/day02/src/main.rs
+++ 2020/day02/src/main.rs
@@ -7,4 +7,19 @@ use advent_lib::*;
 
 fn main() {
     let lines: Vec<String> = advent_lib::file_to_vec("input.txt".to_string());
+
+    for line in lines {
+        println!("criteria {:#?}", get_criteria(line));
+    }
+}
+
+fn get_criteria(line: String) -> ((u8, u8), char) {
+    let criteria: String = line.split(':').collect::<Vec<&str>>()[0].to_string();
+    let c: char = criteria.split(' ').collect::<Vec<&str>>()[1].to_string().chars().nth(0).unwrap();
+
+    let nums: Vec<&str> = criteria.split(' ').collect::<Vec<&str>>()[0].split('-').collect::<Vec<&str>>();
+    let min: u8 = nums[0].parse::<u8>().unwrap();
+    let max: u8 = nums[1].parse::<u8>().unwrap();
+
+    return ((min, max), c);
 }
