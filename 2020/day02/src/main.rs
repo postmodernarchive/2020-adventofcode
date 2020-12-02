3486792d27c107ffb28c130a620e3fa36ec2dab7
youngtrashbag <yuule.smeker@gmail.com>
2020-12-02 08:53:27 +0100

    day02: use library, and describe task at hand quickly

--- 2020/day02/src/main.rs
+++ 2020/day02/src/main.rs
@@ -1,3 +1,10 @@
+extern crate advent_lib;
+use advent_lib::*;
+
+// read file line by line
+// each line has criteria if "password" is valid before ("1-3 a:")
+// that means, for the password to be valid, the letter `a` neds to be present at least 1 to 3 times
+
 fn main() {
-    println!("Hello, world!");
+    let lines: Vec<String> = advent_lib::file_to_vec("input.txt".to_string());
 }
