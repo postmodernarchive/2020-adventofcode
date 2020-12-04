4e8b75d9d1e3041a61e06acf7f9bc43c15b27150
youngtrashbag <yuule.smeker@gmail.com>
2020-12-04 08:59:47 +0100

    day04: begin implementing struct and describe todays puzzle

---
+++ 2020/day04/src/main.rs
@@ -0,0 +1,55 @@
+extern crate advent_lib;
+
+// passports:
+// key:value pairs
+// pairs are separated by spaces or newlines
+// passports are separated by empty newlines
+// passport contains fields:
+//    byr (Birth Year)
+//    iyr (Issue Year)
+//    eyr (Expiration Year)
+//    hgt (Height)
+//    hcl (Hair Color)
+//    ecl (Eye Color)
+//    pid (Passport ID)
+//    cid (Country ID)
+// all fields are reqiured except cid -> country id, which is optional
+// if any other field than cid is missing -> passport is invalid
+
+
+fn main() {
+    let lines: Vec<String> = advent_lib::file_to_vec("input.txt".to_string());
+
+    //separate lines into vector, separated by empty newlines ("\n")
+}
+
+struct Passport {
+    byr: Option<String>,
+    iyr: Option<String>,
+    eyr: Option<String>,
+    hgt: Option<String>,
+    hcl: Option<String>,
+    ecl: Option<String>,
+    pid: Option<String>,
+    cid: Option<String>,
+}
+
+impl Passport {
+    pub fn Parse(str: String) -> Self {
+        Passport {
+            byr: None,
+            iyr: None,
+            eyr: None,
+            hgt: None,
+            hcl: None,
+            ecl: None,
+            pid: None,
+            cid: None,
+
+        }
+    }
+
+    pub fn is_valid(&self) -> bool {
+        false
+    }
+}
