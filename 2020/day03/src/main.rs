d5255f14ac7f341efb37cf97a1d0c12a3461a87b
youngtrashbag <yuule.smeker@gmail.com>
2020-12-04 08:51:57 +0100

    day03: start, but I was too lazy to finish

---
+++ 2020/day03/src/main.rs
@@ -0,0 +1,20 @@
+extern crate advent_lib;
+
+// how many trees do I encounter, when I go right 3 and down 1
+// for the whole "map"
+// pattern repeats
+
+fn main() {
+    let lines: Vec<String> = advent_lib::file_to_vec("input.txt".to_string());
+
+    for line in lines {
+        for x in 0..2 {
+            if line[x] == '#' {
+
+            }
+        }
+
+    }
+}
+
+fn traverse()
