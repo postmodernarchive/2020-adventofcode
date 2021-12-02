6e2eb6077e62b7897792800338a8c297069af74b
youngtrashbag <yuule.smeker@gmail.com>
2021-12-02 10:37:53 +0100

    feat: add day1 of 2021

---
+++ 2021/01/__main__.py
@@ -0,0 +1,39 @@
+from typing import List
+def count_increases(arr: List[int]) -> int:
+    increases = 0
+    previous_value = arr[0]
+    arr.pop(0)
+    for value in arr:
+        if value > previous_value:
+            increases += 1
+
+        previous_value = value
+
+    return increases
+
+
+if __name__ == '__main__':
+    depths = []
+
+    with open('input.txt', 'r', encoding='UTF-8') as input:
+        for l in input.readlines():
+            depths.append(int(l))
+
+    # part one
+    increases = count_increases(depths)
+    print(increases)
+
+    # part two
+    meas_windows = []
+    for i in range(len(depths) - 2):
+        meas_windows.append((depths[i], depths[i+1], depths[i+2]))
+
+    summed_windows = []
+    for window in meas_windows:
+        total = 0
+        for d in window:
+            total += d
+
+        summed_windows.append(total)
+
+    print(f'window increases: {count_increases(summed_windows)}')
